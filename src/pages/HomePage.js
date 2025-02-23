e95546f9ecaafc10a0801ad95e382babbbaf3031
sanketvyapari <46434174+sanketvyapari@users.noreply.github.com>
2025-02-23 18:07:29 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -97,7 +97,7 @@ const HomePage = () => {
           <div
             className="px-3 pt-3"
             style={{
-              background: "#f2bd52",
+              background: "#a68674",
               borderTopLeftRadius: "0.5rem",
               borderTopRightRadius: "0.5rem",
             }}
